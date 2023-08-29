5ae40768e2307c6e2da71e357f5c87a4201d4810
kidest legesse <adeyhaile09@gmail.com>
2023-08-29 11:38:52 +0300

    dependecy updated

--- src/components/home/HomepageCarousel.tsx
+++ src/components/home/HomepageCarousel.tsx
@@ -10,17 +10,7 @@ const HomepageCarousel = () => {
         alt=""
         style={{ marginTop: '70px' }}
       />
-      <Box
-        className="bg-[#00000082] top-0 right-0 left-0 lg:bottom-16 bottom-16 absolute"
-        // style={{
-        //   backgroundColor: 'rgba(0,0,0,.51)',
-        //   top: '0%',
-        //   right: '0%',
-        //   left: '0%',
-        //   bottom: '60px',
-        //   position: 'absolute',
-        // }}
-      >
+      <Box className="bg-[#00000082] top-0 right-0 left-0 lg:bottom-16 bottom-16 absolute">
         <Text
           className="absolute lg:top-60 top-24 lg:text-sm text-xs  right-5 text-center text-[#fff]"
           style={{
