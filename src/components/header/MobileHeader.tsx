f181a5800edca09f54e8652792392d645947d855
kidest legesse <adeyhaile09@gmail.com>
2023-08-29 16:43:51 +0300

    modify

--- src/components/header/MobileHeader.tsx
+++ src/components/header/MobileHeader.tsx
@@ -18,7 +18,7 @@ const MobileHeader: React.FC = () => {
         top: 0,
         left: 0,
         width: '100%',
-        height: '19%',
+        height: '7%',
         zIndex: 100,
         backgroundColor: '#fff',
       }}
